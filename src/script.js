cb824b6be6ab00e0f7ff617abb655d638ac3be75
kernelmemdump <sebastiannovak145@gmail.com>
2017-08-18 20:26:02 +0200

    Initial commit, src files for calculator

---
+++ src/script.js
@@ -0,0 +1,79 @@
+/************************************************************************************************
+ * 
+ * LE amazing calculator 
+ * QA Example
+ * 
+ * As soon as the document(page) is loaded, appropriate event handlers are set on 
+ * all operation buttons representing CALCULATOR OPERATIOSN THAT CAN BE PERFORMED.
+ * 
+ * We also have an onchange event handler set for OPERATOR A and OPERATOR B:
+ *  --->When a user enters a value greater than 100, he has to be informed that the 
+ *      calculation might take a long time
+ * 
+ * 
+ * 
+ * *********************************************************************************************/
+ 
+ 
+ var gLastResult = 0;
+ var gCurrentBackgroundColor = "white";
+ 
+ //////////////////////////////////////////
+ // UI FIELDS OF INTEREST               //
+ /////////////////////////////////////////
+ var gTxtOperatorA = null;
+ var gTxtOperatorB = null;
+ var gTxtResult = null;
+ /////////////////////////////////////////
+ 
+ 
+ 
+ 
+ /**
+  * Set up event handlers for clicks and changes to OPERATOR A and Operator B
+  */
+ window.onload = function() {
+   
+ };
+ 
+ 
+ 
+  //////////////////////////////////////////
+ // EVENT HANDLERS                       //
+ /////////////////////////////////////////
+ 
+ 
+ /**
+  * called when + operation is clicked
+  */
+ function doAddition() {
+   
+ }
+ 
+  
+ /**
+  * called when + operation is clicked
+  */
+ function doSubstration() {
+   
+ }
+
+ /**
+  * called when + operation is clicked
+  */
+ function doAddition() {
+   
+ }
+ 
+ 
+ /**
+  * called when + operation is clicked
+  */
+ function doAddition() {
+   
+ }
+ 
+  
+ 
+ 
+ 
